26c5d181dd0d1883c09fdb7df1f77390031f32fe
OlenaKononenko23 <elenakononenko91@ukr.net>
2024-05-04 17:38:32 +0300

    1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,13 @@
 import Description from "../Description/Description";
 import Options from "../Options/Options";
+import Feedback from "../Feedback/Feedback";
 
 export default function App() {
   return (
     <>
       <Description />
       <Options />
+      <Feedback />
     </>
   )
 }
---
+++ src/components/Feedback/Feedback.jsx
@@ -0,0 +1,9 @@
+export default function Feedback() {
+    return (
+        <>
+            <p>Good: 0</p>
+            <p>Neutral: 0</p>
+            <p>Bad: 0</p>
+        </>
+    )
+}
